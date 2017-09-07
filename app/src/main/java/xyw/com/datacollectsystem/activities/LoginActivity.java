249bc467bee46cccf2ce00b9e362bc45a362b8b5
XYW <314290357@qq.com>
2017-09-07 16:22:56 +0800

    09-07 dev.1

---
+++ app/src/main/java/xyw/com/datacollectsystem/activities/LoginActivity.java
@@ -0,0 +1,43 @@
+package xyw.com.datacollectsystem.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import xyw.com.datacollectsystem.BaseActivity;
+
+/**
+ * Created by 31429 on 2017/9/7.
+ */
+
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void findViewByID() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
